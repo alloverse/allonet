1d08bb80f8c4bd9106939fe2004539446cd55911
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-11-11 23:10:37 +0100

    damn clocks

--- examples/dummyserv/dummyserv.c
+++ examples/dummyserv/dummyserv.c
@@ -2,18 +2,25 @@
 #include <stdio.h>
 #include <time.h>
 
+double gettime() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec/(double)1e9;
+}
+
 int main(int argc, char **argv) {
     printf("hello world\n");
     alloserver *serv = allo_listen();
 
-    clock_t time_per_frame = CLOCKS_PER_SEC/20;
+    double time_per_frame = 1/20.0;
 
     for(;;) {
-        clock_t frame_start = clock();
+        double frame_start = gettime();
         for(;;) {
-            clock_t remaining = (frame_start + time_per_frame) - clock();
-            if(remaining < 0) break;
-            serv->interbeat(serv, remaining / CLOCKS_PER_SEC);
+            double elapsed = gettime() - frame_start;
+            double remaining = time_per_frame - elapsed;
+            if(elapsed > time_per_frame) break;
+            serv->interbeat(serv, remaining*1000);
         }
         serv->beat(serv);
     }

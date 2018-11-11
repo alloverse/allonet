1229f8f6f4bbf2ac60b2d65f0fd220a486d7bcab
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-11-11 23:26:51 +0100

    mmh this is getting tedious. c is tedious.

--- examples/dummyserv/dummyserv.c
+++ examples/dummyserv/dummyserv.c
@@ -1,16 +1,30 @@
 #include <allonet/allonet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-double gettime() {
+double gettime()
+{
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return ts.tv_sec + ts.tv_nsec/(double)1e9;
 }
 
+allo_state dummystate;
+
+static void move_stuff_around(alloserver *serv, double delta)
+{
+    for(int i = 0; i < serv->client_count; i++) {
+        alloserver_client *client = serv->clients[i];
+        // fetch entity, or create if it doesn't have one
+        // move it based on its intent
+    }
+}
+
 int main(int argc, char **argv) {
     printf("hello world\n");
     alloserver *serv = allo_listen();
+    dummystate.entities = (allo_entity*)calloc(128, sizeof(allo_entity));
 
     double time_per_frame = 1/20.0;
 
@@ -22,6 +36,7 @@ int main(int argc, char **argv) {
             if(elapsed > time_per_frame) break;
             serv->interbeat(serv, remaining*1000);
         }
+        move_stuff_around(serv, time_per_frame);
         serv->beat(serv);
     }
 

5459f58d2aa57af09f658c5fa47cdc2baf7c5cb0
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2019-10-19 21:27:05 +0200

    Use explicit weak symbols instead of -undefined suppress

--- lang/lua/allonet-lua.c
+++ lang/lua/allonet-lua.c
@@ -5,7 +5,15 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
-static int l_sin (lua_State *L) {
+// Weak link Lua so that we can expose a Lua API without requiring to link lua
+// in all h ost processes.
+extern lua_Number lua_tonumber (lua_State *L, int index) __attribute__((weak));
+extern void lua_pushnumber (lua_State *L, double n) __attribute__((weak));
+void luaL_register (lua_State *L,
+                    const char *libname,
+                    const luaL_Reg *l) __attribute__((weak));
+
+int l_sin (lua_State *L) {
     double d = lua_tonumber(L, 1);  /* get argument */
     lua_pushnumber(L, sin(d));  /* push result */
     return 1;  /* number of results */
@@ -17,6 +25,6 @@ static const struct luaL_reg allonet [] = {
     };
 
 int luaopen_allonet (lua_State *L) {
-    luaL_openlib(L, "allonet", allonet, 0);
+    luaL_register (L, "allonet", allonet);
     return 1;
 }

6db499f743e1510c835fdd1c4579c092be59d4e2
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2019-10-20 19:58:10 +0200

    disable allonet-lua in windows
    until we know how to build it

--- lang/lua/allonet-lua.c
+++ lang/lua/allonet-lua.c
@@ -5,13 +5,18 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
+// Todo: figure out how to do this in Windows without requiring lua symbols in the running host
+#ifndef _WIN32
+
+#define WEAK_ATTR __attribute__((weak))
+
 // Weak link Lua so that we can expose a Lua API without requiring to link lua
 // in all h ost processes.
-extern lua_Number lua_tonumber (lua_State *L, int index) __attribute__((weak));
-extern void lua_pushnumber (lua_State *L, double n) __attribute__((weak));
+extern lua_Number lua_tonumber (lua_State *L, int index) WEAK_ATTR;
+extern void lua_pushnumber (lua_State *L, double n) WEAK_ATTR;
 void luaL_register (lua_State *L,
                     const char *libname,
-                    const luaL_Reg *l) __attribute__((weak));
+                    const luaL_Reg *l) WEAK_ATTR;
 
 int l_sin (lua_State *L) {
     double d = lua_tonumber(L, 1);  /* get argument */
@@ -28,3 +33,5 @@ int luaopen_allonet (lua_State *L) {
     luaL_register (L, "allonet", allonet);
     return 1;
 }
+
+#endif

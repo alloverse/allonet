c1fd0afea7f983ae1c02149cabd2e00e51afe806
Patrik Sjöberg <voxxar@gmail.com>
2020-12-17 09:08:20 +0100

    try again

--- src/assetstore.c
+++ src/assetstore.c
@@ -80,7 +80,6 @@ static int64_t allo_pwrite(int fd, const void *buf, size_t size, off_t offset)
     overlapped.Offset = offset;
     if (!WriteFile(handle, buf, size, &result, &overlapped))
     {
-        _dosmaperr(GetLastError());
         return -1;
     }
     

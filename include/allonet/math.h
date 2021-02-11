a895e13cd0ddb926c1b4c01accfd1046afb98747
Nevyn Bengtsson <nevyn@alloverse.com>
2021-02-11 22:15:55 +0100

    this is a gnuism, it won't ever compile in Windows

--- include/allonet/math.h
+++ include/allonet/math.h
@@ -10,14 +10,8 @@
 #pragma pack(1)
 
 #ifndef MAX
- #define MAX(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
- #define MIN(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a < _b ? _a : _b; })
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
 #endif
 
 typedef union allo_vector

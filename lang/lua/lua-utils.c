88ad685ce3396e724978530d6eb60253afab4b6e
Nevyn Bengtsson <nevyn@alloverse.com>
2021-04-06 22:03:28 +0200

    nice little convenience

--- lang/lua/lua-utils.c
+++ lang/lua/lua-utils.c
@@ -2,6 +2,32 @@
 #include <string.h>
 
 ////// convenience functions
+
+// useful debugging tool: https://stackoverflow.com/a/59097940
+static void dumpstack (lua_State *L) {
+  int top=lua_gettop(L);
+  for (int i=1; i <= top; i++) {
+    printf("%d\t%s\t", i, luaL_typename(L,i));
+    switch (lua_type(L, i)) {
+      case LUA_TNUMBER:
+        printf("%g\n",lua_tonumber(L,i));
+        break;
+      case LUA_TSTRING:
+        printf("%s\n",lua_tostring(L,i));
+        break;
+      case LUA_TBOOLEAN:
+        printf("%s\n", (lua_toboolean(L, i) ? "true" : "false"));
+        break;
+      case LUA_TNIL:
+        printf("%s\n", "nil");
+        break;
+      default:
+        printf("%p\n",lua_topointer(L,i));
+        break;
+    }
+  }
+}
+
 /* assume that table is on the stack top */
 char *get_table_string(lua_State *L, const char *key)
 {

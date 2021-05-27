1c8b3799c844a7da3b3e18722243c250319ab324
Nevyn Bengtsson <nevyn@alloverse.com>
2021-05-27 14:17:37 +0200

    c linkage

--- src/video/mjpeg.cpp
+++ src/video/mjpeg.cpp
@@ -3,12 +3,12 @@
 #include <richgel9999-jpegcompressor/jpgd.h>
 #include <richgel9999-jpegcompressor/jpge.h>
 
-void allo_mjpeg_encode(allopixel *pixels, int32_t pixels_wide, int32_t pixels_high, allo_mjpeg_encode_func encoder, void *ctx)
+extern "C" void allo_mjpeg_encode(allopixel *pixels, int32_t pixels_wide, int32_t pixels_high, allo_mjpeg_encode_func encoder, void *ctx)
 {
     tje_encode_with_func((tje_write_func*)encoder, ctx, 1, pixels_wide, pixels_high, 4, (const unsigned char*)pixels);
 }
 
-allopixel *allo_mjpeg_decode(uint8_t *jpegdata, size_t jpegdata_size, int32_t *pixels_wide, int32_t *pixels_high)
+extern "C" allopixel *allo_mjpeg_decode(uint8_t *jpegdata, size_t jpegdata_size, int32_t *pixels_wide, int32_t *pixels_high)
 {
     int components;
     allopixel *pixels = (allopixel*)jpgd::decompress_jpeg_image_from_memory(jpegdata, jpegdata_size, (int*)pixels_wide, (int*)pixels_high, &components, 4, 0);
--- src/video/mjpeg.h
+++ src/video/mjpeg.h
@@ -1,6 +1,14 @@
 #include <allonet/client.h>
 
+#ifdef __cplusplus
+ extern "C" { 
+#endif
+
 typedef void allo_mjpeg_encode_func(void *ctx, void* encoded, int size);
 
 void allo_mjpeg_encode(allopixel *pixels, int32_t pixels_wide, int32_t pixels_high, allo_mjpeg_encode_func *encoder, void *ctx);
 allopixel *allo_mjpeg_decode(uint8_t *jpegdata, size_t jpegdata_size, int32_t *pixels_wide, int32_t *pixels_high);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif

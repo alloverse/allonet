70e22202ab7854c89ee53528feeacc574ee55b65
Patrik Sjöberg <voxxar@gmail.com>
2021-06-07 12:07:16 +0200

    luaL_optint

--- lang/lua/allonet-lua.c
+++ lang/lua/allonet-lua.c
@@ -205,7 +205,7 @@ static int l_alloclient_send_video(lua_State *L)
     size_t width = luaL_checkint(L, 4);
     size_t height = luaL_checkint(L, 5);
     size_t format = luaL_optstring(L, 6, "rgba");
-    size_t stride = luaL_optinteger(L, 7, 0);
+    size_t stride = luaL_optint(L, 7, 0);
     if (strcmp(format, "rgba") == 0) {
         alloclient_send_video(lclient->client, track_id, (allopixel*)data, width, height);
     } else if (strcmp(format, "bgrx8") == 0) {

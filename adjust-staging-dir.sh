ed4dd8db85ec29334a9de032b7a1d53f23b05d80
Nevyn Bengtsson <nevyn@alloverse.com>
2021-11-30 14:22:04 +0100

    put meta.json on s3

--- adjust-staging-dir.sh
+++ adjust-staging-dir.sh
@@ -3,6 +3,7 @@ set -ex
 VERSION=`cat build/include/allonet_version.txt | xargs echo -n`
 STAGE=$1
 PLATFORM=$2
+BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 if [ -z "$STAGE/version" ];
 then
@@ -10,5 +11,23 @@ then
     exit 1
 fi
 
+generate_metadata()
+{
+  cat <<EOF
+{
+  "version": "${VERSION}",
+  "platform": "${PLATFORM}",
+  "branch": "${BRANCH}",
+  "buildid": "${BUILD_BUILDID}",
+  "buildnumber": "${BUILD_BUILDNUMBER}",
+  "sourcebranch": "${BUILD_SOURCEBRANCHNAME}",
+  "githash": "${BUILD_SOURCEVERSION}",
+  "changemsg": "${BUILD_SOURCEVERSIONMESSAGE}"
+}
+EOF
+}
+
+echo "$(generate_metadata)" > $STAGE/${BRANCH}_${PLATFORM}.json
+exit 0
 mv "$STAGE/version/build" "$STAGE/version/$PLATFORM"
 mv "$STAGE/version" "$STAGE/$VERSION"
